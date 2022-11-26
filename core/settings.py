2a88e50768f9ab710c81c9b3a8a3f119160d095c
luisnavedag <luisjosenavedagarcia@gmail.com>
2022-11-26 19:22:45 +0530

    Mini Blog

--- core/settings.py
+++ core/settings.py
@@ -110,7 +110,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR,'static']
-STATIC_ROOT = [BASE_DIR,'staticfiles_build','static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
